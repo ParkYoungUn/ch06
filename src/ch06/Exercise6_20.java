365f01b6232183c2663b9fb22b808e37e76f869c
ParkYoungUn <youngun45@naver.com>
2020-08-11 16:40:44 +0900

    6_20 문제 해결

--- src/ch06/Exercise6_20.java
+++ src/ch06/Exercise6_20.java
@@ -3,6 +3,17 @@ package ch06;
 public class Exercise6_20 {
 
 	/*max메서드 작성*/
+	static int max(int[] arr) {
+		if(arr == null || arr.length==0)
+			return -99999;
+		int max = arr[0];
+		for (int i=0 ; i < arr.length ; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		return max;
+	}
 	
 	public static void main(String[] args) {
 		int[] data = {3,2,9,4,7};
