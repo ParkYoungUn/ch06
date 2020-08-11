83c5826b8ef4b052346f77ec57ac73d34066e08e
ParkYoungUn <youngun45@naver.com>
2020-08-11 17:04:47 +0900

    6_21 수정 (답지 답안 추가)

--- src/ch06/Exercise6_21.java
+++ src/ch06/Exercise6_21.java
@@ -11,10 +11,11 @@ public class Exercise6_21 {
 	
 	public static void main(String[] args) {
 		int value = 5;
-		final int a;
+	
 		System.out.println(value+ "의 절대값: "+abs(value));
 		value = -10;
 		System.out.println(value+ "의 절대값: "+abs(value));
+		int a;
 	}
 
 }
